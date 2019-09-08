181e6550d6f3b9171b5bfd42d7e5ffa3e39e5090
Alexander Shuey <alex@alexshuey.com>
2019-09-08 19:20:38 -0400

    Removed argument prompting

--- framework/Discord/Command.ts
+++ framework/Discord/Command.ts
@@ -1,8 +1,12 @@
 import {Command as BaseCommand, CommandoClient} from "discord.js-commando";
 import CommandInfo from "./Types/CommandInfo";
+import * as _ from 'lodash'
 
 export default class Command extends BaseCommand {
     constructor(client: CommandoClient, info: CommandInfo) {
+        info = _.extend({
+            argsPromptLimit: 0
+        }, info);
         super(client, info);
     }
 }
