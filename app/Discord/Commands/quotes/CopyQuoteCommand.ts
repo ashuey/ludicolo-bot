4638ea270bd63c0b4b5ce0bb55a1da6c80a3fe08
Alexander Shuey <alex@alexshuey.com>
2019-09-16 10:44:52 -0400

    Added copy quote command

---
+++ app/Discord/Commands/quotes/CopyQuoteCommand.ts
@@ -0,0 +1,48 @@
+import Command from "../../../../framework/Discord/Command";
+import Quote from "../../../Quote";
+import {CommandMessage} from "discord.js-commando";
+import {Message, RichEmbed, Role} from "discord.js";
+
+export default class DelQuoteCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'copyquote',
+            aliases: ['copy-quote'],
+            group: 'quotes',
+            memberName: 'copyquote',
+            description: 'Copies a quote from another server to this one',
+            guildOnly: true,
+            ownerOnly: true,
+
+            args: [
+                {
+                    key: 'qid',
+                    label: 'ID',
+                    prompt: '',
+                    type: 'integer'
+                }
+            ]
+        })
+    }
+
+    async handle(msg: CommandMessage, { qid }) {
+        const quote = await Quote.query().findById(qid);
+
+        if (!quote) {
+            return this.sendFailedResponse(msg);
+        }
+
+        await Quote.query().insert({
+            guild: msg.guild.id,
+            creator: quote.creator,
+            name: quote.name,
+            text: quote.text
+        });
+
+        return msg.embed(new RichEmbed().setColor('GREEN').setTitle(`**${msg.author.username}** Copied quote #${quote.id}: ${quote.name} to this server.`));
+    }
+
+    protected sendFailedResponse(msg: CommandMessage): Promise<Message | Message[]> {
+        return msg.embed(new RichEmbed().setColor('RED').setTitle(`**${msg.author.username}** No quote with that ID exists`));
+    }
+}
