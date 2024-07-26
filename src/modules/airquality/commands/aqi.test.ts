fed3289e963c4fa092932bd0eebaaf7c37953d2e
Alex Shuey <alex@alexshuey.com>
2024-07-26 15:09:19 -0400

    Added tests for AQI command

---
+++ src/modules/airquality/commands/aqi.test.ts
@@ -0,0 +1,48 @@
+import {AQICommand} from "@/modules/airquality/commands/aqi";
+import {AirNow} from "@/modules/airquality/airnow";
+
+class AirNowTest extends AirNow {
+    private callIdx = 0;
+
+    private responses: unknown[] = [
+        [{"DateObserved":"2024-07-26","HourObserved":13,"LocalTimeZone":"EST","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"O3","AQI":28,"Category":{"Number":1,"Name":"Good"}},{"DateObserved":"2024-07-26","HourObserved":13,"LocalTimeZone":"EST","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"PM2.5","AQI":37,"Category":{"Number":1,"Name":"Good"}}],
+        [{"DateIssue":"2024-07-26","DateForecast":"2024-07-26","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"O3","AQI":32,"Category":{"Number":1,"Name":"Good"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-26","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"PM2.5","AQI":38,"Category":{"Number":1,"Name":"Good"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-27","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"O3","AQI":48,"Category":{"Number":1,"Name":"Good"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-27","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"PM2.5","AQI":53,"Category":{"Number":2,"Name":"Moderate"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-28","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"O3","AQI":51,"Category":{"Number":2,"Name":"Moderate"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-28","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"PM2.5","AQI":55,"Category":{"Number":2,"Name":"Moderate"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-29","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"O3","AQI":51,"Category":{"Number":2,"Name":"Moderate"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-29","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"PM2.5","AQI":60,"Category":{"Number":2,"Name":"Moderate"},"ActionDay":false,"Discussion":""}],
+        [{"DateIssue":"2024-07-26","DateForecast":"2024-07-27","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"O3","AQI":48,"Category":{"Number":1,"Name":"Good"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-27","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"PM2.5","AQI":53,"Category":{"Number":2,"Name":"Moderate"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-28","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"O3","AQI":51,"Category":{"Number":2,"Name":"Moderate"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-28","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"PM2.5","AQI":55,"Category":{"Number":2,"Name":"Moderate"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-29","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"O3","AQI":51,"Category":{"Number":2,"Name":"Moderate"},"ActionDay":false,"Discussion":""},{"DateIssue":"2024-07-26","DateForecast":"2024-07-29","ReportingArea":"Eastern Lake Ontario Region","StateCode":"NY","Latitude":43.5263,"Longitude":-76.8398,"ParameterName":"PM2.5","AQI":60,"Category":{"Number":2,"Name":"Moderate"},"ActionDay":false,"Discussion":""}],
+    ]
+
+    setupTests() {
+        const adapterFn = jest.fn(async (config) => {
+            const responseData = this.responses[this.callIdx];
+            this.callIdx += 1;
+            return {
+                data: responseData,
+                status: 200,
+                statusText: '',
+                headers: {},
+                config: config
+            }
+        });
+        this.http.defaults.adapter = adapterFn;
+        return adapterFn
+    }
+}
+
+describe('aqi command', () => {
+    it('returns the expected response', async () => {
+        const replyFn = jest.fn();
+        const airNow = new AirNowTest('');
+        airNow.setupTests()
+        const cmd = new AQICommand({
+            airNow,
+        });
+
+        await cmd.execute({
+            reply: replyFn,
+            options: {
+                getInteger: () => 14612,
+            }
+        } as never);
+
+        expect(replyFn.mock.calls[0][0].embeds[0].toJSON()).toMatchSnapshot();
+    })
+})
