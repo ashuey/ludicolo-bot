2c4f95e466eeef80292734e39eff9a1c649f7e53
Alex Shuey <alex@alexshuey.com>
2025-05-01 15:12:38 -0400

    Fix migration bug

--- src/modules/ffxiv/index.ts
+++ src/modules/ffxiv/index.ts
@@ -1,11 +1,17 @@
+import { Knex } from "knex";
 import { Application } from "@/common/Application";
 import { ThunderGodCommand } from "@/modules/ffxiv/cmd/thundergod";
 import { XIVCommand } from "@/modules/ffxiv/cmd/xiv";
 import { ApplicationProvider } from "@/common/ApplicationProvider";
 import { Module } from "@/common/Module";
+import { create_static_data_table } from "@/modules/ffxiv/migrations/create_static_data_table";
 
 export class FFXIVModule implements Module, ApplicationProvider {
     readonly name = 'ffxiv';
+
+    readonly migrations: [string, Knex.Migration['up']][] = [
+        ['create_static_data_table', create_static_data_table]
+    ]
 
     readonly commands = [
         new XIVCommand(),
